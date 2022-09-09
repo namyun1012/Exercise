04e10f85cb818a325ef81caac8b761155787caf3
namyun1012 <keukki2@gmail.com>
2022-09-09 20:59:49 +0900

    9/9

---
+++ BAEKJOON/Silver/11279.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+
+int main( ) {
+    std::ios::sync_with_stdio(0); // time code
+    std::cin.tie(0);
+    std::priority_queue<int> que;
+    int N;
+    std::cin >> N;
+    
+    while(N--) {
+        int x;
+        std::cin >> x;
+        if(x == 0) {
+            if(que.empty()) {
+                std::cout << 0 << "\n";
+                continue;
+            }
+            std::cout << que.top() << "\n";
+            que.pop();
+        }
+
+        else {
+            que.push(x);
+        }
+
+
+    }
+
+    return 0;
+}
