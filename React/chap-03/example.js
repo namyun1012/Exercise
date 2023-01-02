32f9fccdaa4e1e06e011dc31f5051bc12c27c961
namyun1012 <keukki2@gmail.com>
2023-01-02 14:33:30 +0900

    01/02 New Year

---
+++ React/chap-03/example.js
@@ -0,0 +1,20 @@
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Inje',
+    lastName: 'Lee'
+}
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}
+    </h1>
+)
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+)
